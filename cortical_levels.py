f1c9958c1971c70d8571265a19bb0758b9137662
Stephen Mann <stephenfmann@gmail.com>
2021-04-16 12:02:31 +0200

    Added cortical_levels and updated README

---
+++ cortical_levels.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 12:00:14 2021
+
+@author: ste_m
+
+Helper functions for cortical.py.
+
+Defines the way in which different causes v_i are generated from level i+1.
+"""
+
+
+
